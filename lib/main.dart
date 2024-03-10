5f2309e2d832a8976a3bebcff643789aa2f07464
bilal-ahmet <akpinarbilalahmet@gmail.com>
2024-03-10 17:48:15 +0300

    weather_app

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_bloc_api/pages/home_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: HomePage(),
+    );
+  }
+}
